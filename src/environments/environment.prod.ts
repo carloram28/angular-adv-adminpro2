99388d450afa39ae23bb0917c02dae90249c6b1d
Carlo Ramirez <carloramirez1295@gmail.com>
2024-06-26 09:38:04 -0500

    childroutes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 //TODO:cambiar el url de los servicios
 export const environment = {
-  production: false,
-  base_url: 'http://localhost:3000/api'
+  production: true,
+  base_url: 'https://adminprobackend2-846a8a5729b3.herokuapp.com/api'
 };
